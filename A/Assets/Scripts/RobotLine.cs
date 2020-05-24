6a1cfcbc08e8dc1d34860ff813680666a4223623
baramyu <45654851+baramyu@users.noreply.github.com>
2020-05-24 18:10:55 +0900

    a

---
+++ A/Assets/Scripts/RobotLine.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotLine : MonoBehaviour
+{
+    public Vector3 startPos;
+    public float endtPoint;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector3.forward * speed * Time.fixedDeltaTime);
+        if (transform.position.x >= endtPoint)
+        {
+            transform.position = startPos;
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ A/Assets/Scripts/RotateObj.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObj : MonoBehaviour
+{
+    public float speed;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    private void FixedUpdate()
+    {
+        transform.Rotate(Vector3.up * speed * Time.fixedDeltaTime);
+    }
+}
